fb382ceb7c035b342ac6aec450b54dfd4549cba4
Guillaume <guillaumehuardhughes@gmail.com>
2021-06-17 18:02:23 -0400

    Flip image

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const fs = require("fs");
+var path = require("path");
 const jimp = require("jimp");
 
 const imagesFolder = "./images/";
 
-async function flip(path, image) {
-  const jimpImage = await jimp.read(`${path}${image}`);
-
-  jimpImage.flip();
-  //   jimpImage.mirror();
-  //   jimpImage.flip(false, true);
+async function flip(filePath) {
+  const jimpImage = await jimp.read(filePath);
+  jimpImage.flip(true, false).write(filePath);
 }
 
 fs.readdir(imagesFolder, (err, files) => {
   files.forEach((file) => {
-    flip(imagesFolder, file);
+    var filePath = path.resolve(imagesFolder + file);
+    console.log(filePath);
+    flip(filePath);
   });
 });

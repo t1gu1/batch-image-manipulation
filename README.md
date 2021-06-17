df6de8f3effadaa6d6068ee0d0eeb10bd17ab2b5
Guillaume <guillaumehuardhughes@gmail.com>
2021-06-17 18:12:59 -0400

    Add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Batch images modification
+
+## Install
+
+- Require nodeJS installed (Mine is v15.14.0. I don't test with others versions)
+- Install modules with `npm install`
+
+## How to Run
+
+`npm start`
+
+## How to Use
+
+Put your images in folder images
+
+## How to make other operations
+
+Other jimp operation available here: <https://github.com/oliver-moran/jimp>
+
+Simple add some to the `index.js` file in `editImages` function
--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const fs = require("fs");
 var path = require("path");
 const jimp = require("jimp");
 
+// Path to the folder to edit image
 const imagesFolder = "./images/";
 
-async function flip(filePath) {
+const editImages = async (filePath) => {
   const jimpImage = await jimp.read(filePath);
-  jimpImage.flip(true, false).write(filePath);
-}
+  jimpImage.flip(true, false);
+  // Other jimp operation available here: https://github.com/oliver-moran/jimp
 
+  return jimpImage;
+};
+
+// Run when program start
 fs.readdir(imagesFolder, (err, files) => {
   files.forEach((file) => {
     var filePath = path.resolve(imagesFolder + file);
-    console.log(filePath);
-    flip(filePath);
+    editImages(filePath).write(filePath);
   });
 });
